7304855290968f2b7381410dc9db8da4c69d397d
Mark Erwin C. Villarina <vimr@erni.ph>
2020-05-30 19:04:55 +0800

    Added

---
+++ SampleWebApp/Models/ImportJsonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IRISGlobalsNative.Extensions.SampleWebApp.Models
+{
+    public class ImportJsonModel
+    {
+        public string JsonValue { get; set; }
+    }
+}
